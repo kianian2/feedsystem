ecb9b790a06d4e0fc2c485ac69041fc7667c781a
BrandonDutcher <brandonbdutcher@gmail.com>
2020-02-19 19:41:13 -0800

    fixed T0

--- helium_pressurization.py
+++ helium_pressurization.py
@@ -6,7 +6,7 @@ def compute_He_T_rise(mat,T0):
     He = Helium(600,600)
 
     V_He = 0.01 # m^3
-    E_He = V_He*He.get_density()*He.get_Cv()*(He.T - mat.T0)
+    E_He = V_He*He.get_density()*He.get_Cv()*(He.T - T0)
     V_Tank = 0.015
     E_LOx = V_Tank*mat.rho*mat.cp
     return E_LOx/E_He
