e7e3be5afb8344c3b130883ed808334b4646c17a
nolanlad <nolanrmccarthy@gmail.com>
2020-02-19 19:40:34 -0800

    engine sim

--- engine_sim.py
+++ engine_sim.py
@@ -6,33 +6,51 @@ Created on Thu Feb 13 20:18:53 2020
 """
 
 from feed import *
+from thermo import get_tank_properties
+from materials import Oxygen, Methane
 
-MR = 2.8;               # (Ox:CH4) mixture ratio
-mdot_tot = 1.22; 
-mdot_LOx = mdot_tot * MR/(MR+1);                # mass flow rate of LOx [kg/s]
-mdot_CH4 = mdot_tot * 1/(MR+1);  
-p_LOx = 1142           # density LOx Based on Pt = 725 psia and T = 100K [kg/m3]
-p_CH4 = 423          # density CH4 Based on P = 473.6 psia and T = [100 111.7]K [kg/m3]
-u_LOx = 202e-6; 
-u_CH4 = 122e-6; 
-TP_LOx = get_PT_LOx(mdot_LOx,p_LOx,u_LOx)
-TP_CH4 = get_PT_CH4(mdot_CH4,p_CH4,u_CH4)
+def get_MR(t_end, pressure_LOx, pressure_CH4):
+    TLOx,TCH4 = get_tank_properties(t_end, pressure_LOx, pressure_CH4)
+    ox = Oxygen(pressure_LOx,TLOx)
+    ch4 = Methane(pressure_CH4,TCH4)
+    rho_LOx = ox.get_density()
+    rho_CH4 = ch4.get_density()
+    visc_LOx = ox.get_viscosity()
+    visc_CH4 = ch4.get_viscosity()
+    mdot_LOX = get_mdot_LOx(pressure_LOx, rho_LOx, visc_LOx)
+    mdot_CH4 = get_mdot_CH4(pressure_CH4, rho_CH4, visc_CH4)
+    MR = mdot_LOX/mdot_CH4
+    return MR
 
-mdot_LOX_2 = get_mdot_LOx(TP_LOx, p_LOx, u_LOx)
-mdot_CH4_2 = get_mdot_CH4(TP_CH4, p_CH4, u_CH4)
+print(get_MR(200,538,461))
 
-TP_range_ox = np.linspace(500,600,100)
-p_range_ox = np.linspace(722,1150,100)
 
-MRs = np.zeros((100,100))
+# MR = 2.8;               # (Ox:CH4) mixture ratio
+# mdot_tot = 1.22; 
+# mdot_LOx = mdot_tot * MR/(MR+1);                # mass flow rate of LOx [kg/s]
+# mdot_CH4 = mdot_tot * 1/(MR+1);  
+# p_LOx = 1142           # density LOx Based on Pt = 725 psia and T = 100K [kg/m3]
+# p_CH4 = 423          # density CH4 Based on P = 473.6 psia and T = [100 111.7]K [kg/m3]
+# u_LOx = 202e-6; 
+# u_CH4 = 122e-6; 
+# TP_LOx = get_PT_LOx(mdot_LOx,p_LOx,u_LOx)
+# TP_CH4 = get_PT_CH4(mdot_CH4,p_CH4,u_CH4)
 
-for i,tp in enumerate(TP_range_ox):
-    for j,p in enumerate(p_range_ox):
-        MRs[i][j] =  get_mdot_LOx(tp, p, u_LOx)/mdot_CH4_2
+# mdot_LOX_2 = get_mdot_LOx(TP_LOx, p_LOx, u_LOx)
+# mdot_CH4_2 = get_mdot_CH4(TP_CH4, p_CH4, u_CH4)
 
-MR = mdot_LOX_2/mdot_CH4_2
-import matplotlib.pyplot as plt
+# TP_range_ox = np.linspace(500,600,100)
+# p_range_ox = np.linspace(722,1150,100)
 
-plt.imshow(MRs)
-plt.colorbar()
-plt.show()
+# MRs = np.zeros((100,100))
+
+# for i,tp in enumerate(TP_range_ox):
+#     for j,p in enumerate(p_range_ox):
+#         MRs[i][j] =  get_mdot_LOx(tp, p, u_LOx)/mdot_CH4_2
+
+# MR = mdot_LOX_2/mdot_CH4_2
+# import matplotlib.pyplot as plt
+
+# plt.imshow(MRs)
+# plt.colorbar()
+# plt.show()
