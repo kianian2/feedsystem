5120faefb9d31bd8d7f43cf49cc91dc97a68b9f6
nolanlad <nolanrmccarthy@gmail.com>
2020-02-19 17:35:46 -0800

    small fix

--- convection.py
+++ convection.py
@@ -2,7 +2,7 @@
 def CylNuT(U,D,mat,Tf):
     ReD = mat.reynolds(U,D)
     Pr  = mat.prandlt()
-    if min(ReD*Pr) < 0.2:
+    if np.min(ReD*Pr) < 0.2:
         print('warn : CylNuT: ReD*Pr < 0.2')
     NuD = 0.3+0.62*ReD**0.5*Pr**(1/3)*(1+(ReD/282000)**0.625)**0.8/(1+(0.4/Pr)**(2/3))**0.25
     h   = NuD*kf/D  
