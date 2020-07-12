45492e41ed0bb4ea26d009d17c180c62b7ace58a
Adam <adamthemodguy@gmail.com>
2020-07-11 22:27:18 -0700

    fixing imports, changing diffuser fit

--- Copy_Pressurization (Adam 7-10).py
+++ Copy_Pressurization (Adam 7-10).py
@@ -12,9 +12,10 @@ using PV= nRT I will update the pressure as a function of moles and the temperat
 I will run the simulation based on a while loop influenced by the pressure difference, and it will stop once the tanks reach the desired pressure. 
 
 ''' 
-from polyfit_diffuser import * 
+from polyfit_diffuser import q_of_p
 from materials import Helium 
 import matplotlib.pyplot as plt
+import numpy as np
 
 pressurant = Helium(540,100)
 
@@ -24,7 +25,7 @@ cv = 0.01593484
 gallons_to_liters = 3.78541;  # [L/gal] This will be used to convert flow rate into liters per minute 
 helium_density_eul = pressurant.get_density() ;  # [g/L] This will be used for a conversion from flow rate into grams/s
 grams_to_moles =  4.003  ;  # [mol/g] This converts our flowrate from grams/s to mol/s 
-v_eul = 5       ;  # [L]   Fixed quantity throughout pressurization 
+v_eul = 25       ;  # [L]   Fixed quantity throughout pressurization 
 P_reg = 540     ;  # [psi] This is a fixed inlet pressure from the regulator 
 P_eul = 14.7    ;  # [psi} This is the INITIAL gauge pressure inside the tank, ambient pressure
 psi_to_atm = 0.068046    ;  # [psi/atm] This converts our gauge pressure of psi to atm when we use PV=nrt, but remember to add 1 atm because we are using gauge pressures; 
@@ -35,7 +36,7 @@ Ti =  310.928       ;  # [K] Ambient room temperature
 Tf =  111.7         ;  # [K] Methane Cryogenic temperature, this is once pressurant reaches thermal equilibrium 
 T = Ti              ;  # [K] Setting up the initial temperature of our pressurant, it is at ambient temperature (assumption) 
 t = 0               ;  # [s] Setting up euler time step problem
-dt = 0.02            ;  # [s] Setting up time step 
+dt = 0.001            ;  # [s] Setting up time step 
 R  = 0.08205        ;  # [L atm/mol k]
 moles = P_eul*v_eul/(R*T)         ;  # [mol] Initial moles in the tank of pressurant 
 lamb = 1.6667; 
@@ -61,15 +62,15 @@ while P_diff > 1:
     p_f_array.append(T)
     moles_array.append(moles)
     #helium_density_eul = Helium(P_eul,T).get_density()
-    mdot = func(P_diff,cv)*gallons_to_liters*helium_density_eul*grams_to_moles/60 # [mol/s] Based on the curve fit as a function of pressure difference from the company 
+    mdot = q_of_p(P_diff)*gallons_to_liters*helium_density_eul*grams_to_moles/60 # [mol/s] Based on the curve fit as a function of pressure difference from the company 
     moles = moles + mdot * dt
-    #new_press = (moles*R*T/v_eul)/psi_to_atm
-    #T = (T * moles + T_gas(new_press, P_eul) * mdot * dt)/moles #[K] Temperature averaging
+    new_press = (moles*R*T/v_eul)/psi_to_atm
+    T = (T * (moles - mdot * dt) + T_gas(new_press, P_eul) * mdot * dt)/moles #[K] Temperature averaging
     #T = T_gas(new_press, P_eul)
-    #P_eul = new_press          
+    P_eul = new_press          
     temp_pressure_var = converging_temp_pressure(moles,T,P_eul)                                         # [mol/s] This updates our moles inside the eulage volume and prepares for pv=nrt                                          
-    T = temp_pressure_var[0]
-    P_eul = temp_pressure_var[1]
+    #T = temp_pressure_var[0]
+    #P_eul = temp_pressure_var[1]
     P_diff =  P_reg - P_eul                                                   # [psi] Preparation to update mdot                                                      # [k] Use the function inside of the BETE pdf to update the temperature inside the tank as a function of the eulage pressure
     t = t + dt                                                                # [s] updating the time step 
     
--- polyfit_diffuser.py
+++ polyfit_diffuser.py
@@ -2,22 +2,27 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.optimize import curve_fit
 
-datapoints = np.array([[10,.05],
-                       [40,.1],
-                       [85,.15],
-                       [200,.225],
-                       [300,.275]])
-
+disp = False
+datapoints = np.array([[12.5,2],
+                      [30,4],
+                      [50,5.5],
+                      [100,7.8],
+                      [200,11],
+                      [300,13.5]])
 x = datapoints[:,0]
 y = datapoints[:,1]
 
-def func(x,cd):
+def q_of_p(x,cd):
     return cd * x**(1/2)
 
-params = curve_fit(func, x, y)
+params = curve_fit(q_of_p, x, y)
 
-xarr = np.linspace(0,300,100)
-plt.plot(x,y,'o')
-plt.plot(xarr, func(xarr,params[0]))
-print("CV is {}gpm/psi with covariance {}".format(params[0],params[1]))
-plt.show()
+def q_of_p(x):
+    return params[0] * x**(1/2)
+
+if(disp):
+    xarr = np.linspace(0,300,100)
+    plt.plot(x,y,'o')
+    plt.plot(xarr, q_of_p(xarr))
+    print("CV is {}gpm/psi with covariance {}".format(params[0],params[1]))
+    plt.show()
